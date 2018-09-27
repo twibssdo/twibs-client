4b48163eb92401ff29a5df1bcda3d7fa054ef506
Colin McCarthy <colinmccarthy@Colin-Ms-MacBook-Pro.local>
2018-09-27 01:28:27 -0700

    create Tweeber component

---
+++ src/components/Tweeber.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Image, Button } from 'react-native';
+
+export default ({ tweeber, makeTweeb }) => {
+  // some logic using tweeber data
+  return (
+    <View>
+      <Image source={tweeber.avatarImgUrl} />
+      <Text>{tweeber.fullName}</Text>
+      <Button
+        onPress={() => makeTweeb(tweeber.id)}
+        title="Tweeb this fool"
+        disabled={tweeber.offerMade} />
+    </View>
+  )
+}

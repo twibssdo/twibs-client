02d9a82ede2517ea781e11f07511d309937409cf
Colin McCarthy <colinmccarthy@Colin-Ms-MacBook-Pro.local>
2018-09-27 01:27:50 -0700

    add dummy navbar component

---
+++ src/components/NavBarDummy.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// just a dummy navbar for now
+const NavBar = () => (
+  <View style={{width: '100%', height: 100, backgroundColor: 'lightpurple'}}>
+    <Text>{'Navbar Placeholder'}</Text>
+  </View>
+);
+
+export default NavBar;

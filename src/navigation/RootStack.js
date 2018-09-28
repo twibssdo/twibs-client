ea3309b58c2a1acb2c428b08eb2df54680e8b250
Christopher <chris132128@gmail.com>
2018-09-27 23:36:37 -0700

    added LoginScreen to the RootStack

--- src/navigation/RootStack.js
+++ src/navigation/RootStack.js
@@ -1,6 +1,8 @@
 import { createStackNavigator } from 'react-navigation';
+import LoginScreen from '../containers/LoginScreen';
 import HomeScreen from '../containers/HomeScreen';
 
 export default createStackNavigator({
+  Login: LoginScreen,
   Home: HomeScreen,
 });

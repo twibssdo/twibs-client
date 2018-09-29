be705c48ab770ea16f5b6e998b0b6954916e299b
SKChristopher <chris132128@gmail.com>
2018-09-28 23:44:34 -0700

    Ignoring App.js changes
    - Reverted to original without the tweeber component.

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import View from 'react-native';
-import Tweeber from './src/util/tweeber';
 import RootStack from './src/navigation/RootStack';
-import Location from './src/containers/Location';
-
-const dummyTweebers = [new Tweeber(1), new Tweeber(2), new Tweeber(3)];
-dummyTweebers[0].setName('Will', 'Adamowicz');
-dummyTweebers[1].setName('Colin', 'McCarthy');
-dummyTweebers[2].setName('Christopher', 'Washburn');
-const makeTweeb = id => console.log('Somebody tweebed user with id ', id);
 
 export default class App extends React.Component {
   constructor() {
@@ -18,7 +9,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Location tweebers={dummyTweebers} makeTweeb={makeTweeb} />
+      <RootStack />
     );
   }
 }

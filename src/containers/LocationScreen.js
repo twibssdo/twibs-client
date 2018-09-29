bd3ad30dbd638e3e3a64cc6f518544db633af9e0
Christopher <chris132128@gmail.com>
2018-09-29 00:07:14 -0700

    renamed Location to LocationScreen; extracted styles into a styleSheet;

--- src/containers/LocationScreen.js
+++ src/containers/LocationScreen.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import NavBarDummy from '../components/NavBarDummy';
 import Tweeber from '../components/Tweeber';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  tweeber: {
+    height: 50,
+  },
+});
+
 const Location = ({ tweebers, makeTweeb }) => (
-  <View style={{flexDirection: 'column', flex: 1, justifyContent: 'space-between'}}>
-    <View style={{height: 50}} />
+  <View style={styles.container}>
+    <View style={styles.tweeber} />
     {tweebers.map((tweeber, i) => <Tweeber key={`tk${i}`} tweeber={tweeber} makeTweeb={makeTweeb} />)}
     <NavBarDummy />
   </View>

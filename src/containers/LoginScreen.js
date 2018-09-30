11477df992206c5d0d27f12ecf27542a6331330a
William Adamowicz <william.adamowicz@gmail.com>
2018-09-30 00:23:30 -0700

    implement facebook oauth

--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -4,6 +4,7 @@ import {
   StyleSheet, Text, View, Button,
 } from 'react-native';
 import * as colors from '../constants/colors';
+import Keychain from 'react-native-keychain';
 
 const styles = StyleSheet.create({
   container: {
@@ -24,17 +25,42 @@ export default class Login extends React.Component {
   }
 
   authenticateUser = (oAuthProvider) => {
-    console.log(oAuthProvider);
+    async function logIn() {
+      const { type, token } = Expo.Facebook.logInWithReadPermissionsAsync('236471710557928', {
+        permissions: ['email'],  
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`);
+        Alert.alert(
+          'Logged in!',
+          `Hi ${(await response.json()).name}!`,
+        );
+      }
+
+      // Set the token with keychain to securely store data
+      Keychain.setGenericPassword('session', token);
+    }
+
+    logIn();
     this.props.navigation.navigate('Home');
   }
 
+  retrieveToken() {
+    Keychain.getGenericPassword.then(creds => creds.password).then(token => {
+      // use token
+      this.setState({})
+    });
+  }
+  
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>⏱ === 💰</Text>
         <Button
           onPress={() => this.authenticateUser('venmo')}
-          title="Log in with Venmo"
+          title="Logasf in with Venmo"
           color={colors.$venmo}
           accessibilityLabel="Log in with Venmo"
         />

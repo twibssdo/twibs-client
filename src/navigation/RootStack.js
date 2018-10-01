7439da4e4c4f954d5c9c768fb03199fb4362c99b
Colin McCarthy <colinmccarthy@Colin-Ms-MacBook-Pro.local>
2018-09-30 18:26:52 -0700

    add profile screen to stack navigator

--- src/navigation/RootStack.js
+++ src/navigation/RootStack.js
@@ -2,12 +2,14 @@ import { createStackNavigator } from 'react-navigation';
 import LoginScreen from '../containers/LoginScreen';
 import HomeScreen from '../containers/HomeScreen';
 import LocationScreen from '../containers/LocationScreen';
+import ProfileScreen from '../containers/ProfileScreen';
 
 export default createStackNavigator(
   {
     Login: LoginScreen,
     Home: HomeScreen,
     Location: LocationScreen,
+    Profile: ProfileScreen,
   },
   {
     initialRouteName: 'Login',
